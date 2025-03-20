e7f205b782d72dc156aa8c80b104adbebdc6ce5e
meg <megfa@yahoo.com>
2025-03-20 18:03:10 +0100

    Skills

--- src/pages/main/corePages/Skills.tsx
+++ src/pages/main/corePages/Skills.tsx
@@ -7,28 +7,138 @@ export const Skills: React.FC = () => {
     <div className='w-full relative h-[130vh] flex flex-col items-start justify-start'>
 
         <header className=' w-full flex items-start justify-between'>
-            <div className=" w-full bg-center m-0 brightness-90">
-                <img src={'assets/skill.jpg'} alt="skills" />
+            <div className=" w-full h-[40vh] bg-center m-0 brightness-90"
+                style={{
+                    backgroundImage: `url(assets/skill.jpg)`,
+                    backgroundPosition: "bottom",
+                    backgroundSize: "cover"
+                }}
+            >
+                {/* <img src={'assets/skill.jpg'} alt="skills" /> */}
             </div>
-            <div className='z-10 p-30 w-full bg-darkPrimary h-[120%] flex-col text-white items-start text-sm'>
+            <div className='z-10 p-30 w-full bg-darkPrimary h-[45vh] flex-col text-white items-start text-sm'>
                 <span className='text-gray-400'>The only source of knowledge is experience - Albert Einstein</span>
                 <h1 className='text-2xl font-bold mb-12 mt-2 relative tittle'>My 
                     <span className='text-primary text-4xl font-[Titillium] lowercase italic'> developer's </span>and design skills</h1>
                 <p className='text-[12rem] ml-32 text-[#acacac1f] font-black z-10 top-50 absolute rotate-90'>Skills</p>
             </div>
         </header>
-        <main className='grid'>
-            <section>
-                <h1>Developer skills</h1>
-            </section>
-        <div className='flex mt-12'>
-                <div className='educationSection'>
-                    <p>2017 - 2018</p>
-                    <h2>React, JS, Redux Course</h2>
-                    <p>CodersLab</p>
+        <main className='px-20 gridPage w-full '>
+            <h1 className='text-2xl font-bold mt-20 pl-10 relative h-[30px] beforeVertical col-start-1 row-start-1'>Developer skills</h1>
+            <section className='w-full col-start-2 row-start-2 col-end-4 flex flex-col'>
+                <div className='flex flex-col w-full font-medium text-base'>
+                    <div className='flex justify-between'>
+                        <h2>JAVASCRIPT / TYPESCRIPT</h2>
+                        <h2>95%</h2>
+                    </div>
+                    <div className='relative w-full h-2'>
+                        <div className='absolute bg-primary w-[95%] h-2 my-3 '/>
+                        <div className='bg-darkPrimary w-full h-2 my-3 '/>
+                    </div>
+                </div>
+                <div className='mt-8 flex flex-col w-full font-medium text-base'>
+                    <div className='flex justify-between'>
+                        <h2>REACT</h2>
+                        <h2>95%</h2>
+                    </div>
+                    <div className='relative w-full h-2'>
+                        <div className='absolute bg-primary w-[95%] h-2 my-3 '/>
+                        <div className='bg-darkPrimary w-full h-2 my-3 '/>
+                    </div>
+                </div>
+                <div className='mt-8 flex flex-col w-full font-medium text-base'>
+                    <div className='flex justify-between'>
+                        <h2>Mobx</h2>
+                        <h2>90%</h2>
+                    </div>
+                    <div className='relative w-full h-2'>
+                        <div className='absolute bg-primary w-[90%] h-2 my-3 '/>
+                        <div className='bg-darkPrimary w-full h-2 my-3 '/>
+                    </div>
+                </div>
+                <div className='mt-8 flex flex-col w-full font-medium text-base'>
+                    <div className='flex justify-between'>
+                        <h2>Redux</h2>
+                        <h2>85%</h2>
+                    </div>
+                    <div className='relative w-full h-2'>
+                        <div className='absolute bg-primary w-[85%] h-2 my-3 '/>
+                        <div className='bg-darkPrimary w-full h-2 my-3 '/>
+                    </div>
+                </div>
+                <div className='mt-8 flex flex-col w-full font-medium text-base'>
+                    <div className='flex justify-between'>
+                        <h2>GraphQL</h2>
+                        <h2>85%</h2>
+                    </div>
+                    <div className='relative w-full h-2'>
+                        <div className='absolute bg-primary w-[85%] h-2 my-3 '/>
+                        <div className='bg-darkPrimary w-full h-2 my-3 '/>
+                    </div>
                 </div>
                 
-            </div>
+               
+            </section>
+            <section className='w-full col-start-5 row-start-2 col-end-7 flex flex-col'>
+                <div className='flex flex-col w-full font-medium text-base'>
+                    <div className='flex justify-between'>
+                        <h2>JAVASCRIPT / TYPESCRIPT</h2>
+                        <h2>95%</h2>
+                    </div>
+                    <div className='relative w-full h-2'>
+                        <div className='absolute bg-primary w-[95%] h-2 my-3 '/>
+                        <div className='bg-darkPrimary w-full h-2 my-3 '/>
+                    </div>
+                </div>
+                <div className='mt-8 flex flex-col w-full font-medium text-base'>
+                    <div className='flex justify-between'>
+                        <h2>REACT</h2>
+                        <h2>95%</h2>
+                    </div>
+                    <div className='relative w-full h-2'>
+                        <div className='absolute bg-primary w-[95%] h-2 my-3 '/>
+                        <div className='bg-darkPrimary w-full h-2 my-3 '/>
+                    </div>
+                </div>
+                <div className='mt-8 flex flex-col w-full font-medium text-base'>
+                    <div className='flex justify-between'>
+                        <h2>Mobx</h2>
+                        <h2>90%</h2>
+                    </div>
+                    <div className='relative w-full h-2'>
+                        <div className='absolute bg-primary w-[90%] h-2 my-3 '/>
+                        <div className='bg-darkPrimary w-full h-2 my-3 '/>
+                    </div>
+                </div>
+                <div className='mt-8 flex flex-col w-full font-medium text-base'>
+                    <div className='flex justify-between'>
+                        <h2>Redux</h2>
+                        <h2>85%</h2>
+                    </div>
+                    <div className='relative w-full h-2'>
+                        <div className='absolute bg-primary w-[85%] h-2 my-3 '/>
+                        <div className='bg-darkPrimary w-full h-2 my-3 '/>
+                    </div>
+                </div>
+                <div className='mt-8 flex flex-col w-full font-medium text-base'>
+                    <div className='flex justify-between'>
+                        <h2>GraphQL</h2>
+                        <h2>85%</h2>
+                    </div>
+                    <div className='relative w-full h-2'>
+                        <div className='absolute bg-primary w-[85%] h-2 my-3 '/>
+                        <div className='bg-darkPrimary w-full h-2 my-3 '/>
+                    </div>
+                </div>
+                
+               
+            </section>
+                <span className='gridVisibileItem col-start-1'/>
+                <span className='gridVisibileItem col-start-2'/>
+                <span className='gridVisibileItem col-start-3'/>
+                <span className='gridVisibileItem col-start-4'/>
+                <span className='gridVisibileItem col-start-5'/>
+                <span className='gridVisibileItem col-start-6'/>
         </main>
     </div>
     )
