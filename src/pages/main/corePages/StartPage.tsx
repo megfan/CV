afacf7a4b295dc2526a9b3d4543b77418f2c9549
meg <megfa@yahoo.com>
2025-04-12 17:59:49 +0200

    Details correction + adding ContactBar and extra MenuBar

--- src/pages/main/corePages/StartPage.tsx
+++ src/pages/main/corePages/StartPage.tsx
@@ -78,64 +78,63 @@ export const StartPage: React.FC = () => {
             className="relative h-[80%] w-[80%] rounded-xl bg-[#2c2d3240] shadow-lg flex justify-between"
             >
             <div className='flex w-full items-center'>
-            <div className='w-1/2 ml-24'>
-                    <h1 className='font-semibold text-5xl font-[Poppins] leading-[5rem] drop-shadow-[(-18px 17px 20px 0px rgba(0,0,0, 1))]'>
-                        Hi there! <br/>
-                        I'm Meg Staszewska <br/>
-                        <span className='text-primary'>Developer and Designer</span> <br/>
-                    </h1>
-                <div className='buttonMovingRight py-4 px-6 mt-4 bg-[#ffffff21] w-fit uppercase text-sm tracking-widest' >
-                    I create web and graphic design
+                <div className='w-1/2 ml-24'>
+                        <h1 className='font-semibold text-5xl font-[Poppins] leading-[5rem] drop-shadow-[(-18px 17px 20px 0px rgba(0,0,0, 1))]'>
+                            Hi there! <br/>
+                            I'm Meg Staszewska <br/>
+                            <span className='text-primary'>Developer and Designer</span> <br/>
+                        </h1>
+                    <div className='buttonMovingRight py-4 px-6 mt-4 bg-[#ffffff21] w-fit uppercase text-sm tracking-widest' >
+                        I create web and graphic design
+                    </div>
+                </div>
+                <div className='relative flex h-full mt-20 w-1/2 items-center justify-center '>
+                    <motion.div className="w-[5rem] h-[13rem] inset-0 z-30 absolute top-15 right-[40%] left-auto -rotate-5"
+                        style={{
+                            backgroundImage: `url(assets/statue.png)`,
+                            backgroundPosition: "bottom",
+                            backgroundSize: "cover",
+                            }}
+                    />
+                    <motion.div 
+                        className="w-[16rem] h-[10rem] inset-0 z-30 absolute top-[30%] left-18 rotate-20"
+                        style={{
+                            backgroundImage: `url(assets/car.png)`,
+                            backgroundPosition: "center",
+                            backgroundSize: "cover",
+                            y: paralaxCar
+                        }}
+                    />
+                    <motion.div className="w-[15rem] h-[3rem] inset-0 z-30 absolute top-[47%] left-[60%] -rotate-40"
+                        style={{
+                            backgroundImage: `url(assets/brushes.png)`,
+                            backgroundPosition: "right",
+                            backgroundSize: "cover",
+                            y: paralaxbrushes
+                            }}
+                    />
+                    <motion.div 
+                        className="w-[3rem] h-[10rem] inset-0 z-10 absolute top-[10%] right-[25%] rotate-20 left-auto"
+                        style={{
+                            backgroundImage: `url(assets/PaintBrush.png)`,
+                            backgroundPosition: "center",
+                            backgroundSize: "cover",
+                            y: paralaxPaintBrush
+                        }}
+                    />
+                    <motion.div 
+                        className="w-[45rem] h-[25rem] inset-0 z-20"
+                        style={{
+                            backgroundImage: `url(assets/cloud.png)`,
+                            backgroundPosition: "center",
+                            backgroundSize: "cover",
+                            y: paralax1
+                        }}
+                    />
                 </div>
             </div>
-            <div className='relative flex h-full mt-20 w-1/2 items-center justify-center '>
-                <motion.div className="w-[5rem] h-[13rem] inset-0 z-30 absolute top-15 right-[40%] left-auto -rotate-5"
-                    style={{
-                        backgroundImage: `url(assets/statue.png)`,
-                        backgroundPosition: "bottom",
-                        backgroundSize: "cover",
-                        }}
-                />
-                <motion.div 
-                    className="w-[16rem] h-[10rem] inset-0 z-30 absolute top-[30%] left-18 rotate-20"
-                    style={{
-                        backgroundImage: `url(assets/car.png)`,
-                        backgroundPosition: "center",
-                        backgroundSize: "cover",
-                        y: paralaxCar
-                    }}
-                />
-                <motion.div className="w-[15rem] h-[3rem] inset-0 z-30 absolute top-[47%] left-[60%] -rotate-40"
-                    style={{
-                        backgroundImage: `url(assets/brushes.png)`,
-                        backgroundPosition: "right",
-                        backgroundSize: "cover",
-                        y: paralaxbrushes
-                        }}
-                />
-                <motion.div 
-                    className="w-[3rem] h-[10rem] inset-0 z-10 absolute top-[10%] right-[25%] rotate-20 left-auto"
-                    style={{
-                        backgroundImage: `url(assets/PaintBrush.png)`,
-                        backgroundPosition: "center",
-                        backgroundSize: "cover",
-                        y: paralaxPaintBrush
-                    }}
-                />
-                <motion.div 
-                    className="w-[45rem] h-[25rem] inset-0 z-20"
-                    style={{
-                        backgroundImage: `url(assets/cloud.png)`,
-                        backgroundPosition: "center",
-                        backgroundSize: "cover",
-                        y: paralax1
-                    }}
-                />
-            </div>
-            {/* </div> */}
-            </div>
         </motion.div >
-    </motion.div>
+      </motion.div>
         <span className='gridVisibileItem col-start-1'/>
         <span className='gridVisibileItem col-start-2'/>
         <span className='gridVisibileItem col-start-3'/>
